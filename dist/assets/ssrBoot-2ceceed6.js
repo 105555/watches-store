8769bc788764bca0117653bbd4593b39e616f9ec
105555 <fter2257@gmail.com>
2024-06-27 12:39:59 +0800

    updates

--- dist/assets/ssrBoot-2ceceed6.js
+++
@@ -1,1 +0,0 @@
-import{s,o as t,b as e,aq as n}from"./index-c2b80dfa.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
---
+++ dist/assets/ssrBoot-f28015f9.js
@@ -0,0 +1,1 @@
+import{s,o as t,b as e,as as n}from"./index-7c7db64b.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
