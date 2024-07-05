4cf5f2a43dee904352ebc7339d24c6b094b534e6
105555 <fter2257@gmail.com>
2024-07-05 15:27:13 +0800

    Remove dist folder from version control

--- dist/assets/lazy-0b2c4299.js
+++
@@ -1,1 +0,0 @@
-import{p as n,s as r,q as u,A as l}from"./index-19afddfb.js";const c=n({eager:Boolean},"lazy");function m(e,o){const a=r(!1),s=u(()=>a.value||e.eager||o.value);l(o,()=>a.value=!0);function t(){e.eager||(a.value=!1)}return{isBooted:a,hasContent:s,onAfterLeave:t}}export{c as m,m as u};
